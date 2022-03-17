1f806f084f765db0d3db7e6241c6ff4f1b13f7c5
HoriCap997 <horicap997@gmail.com>
2022-03-17 11:21:19 +0800

    Complete repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    let words= '';
+    if (times<0){
+        return "ERROR";
+    }
+    for (let i=0; i<times; i++){ 
+        words+=word;
+    }
+    return words;
 };
 
+/* Another Solution
+const repeatString = function(word,number) {
+    const words= new Array();
+    if (number<0){
+        return "ERROR";
+    }
+    for (let i=number; i>0; i--){ 
+        words.push(word);
+    }
+    return words.join("");
+};
+*/
 // Do not edit below this line
 module.exports = repeatString;
