24eb72ea4d19010cafecb503bb5de2633fca5b3b
HoriCap997 <horicap997@gmail.com>
2022-05-11 15:27:20 +0800

    Add Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (arr) {
+    // Keep only alphabet character
+    const onlyAlphabet = arr.replace(/[^a-z]/gi,'').toLowerCase()
 
+    // Check with reverse alphabet only character
+    return onlyAlphabet == onlyAlphabet.split('').reverse().join('')  
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+// const fibonacci = function(n) {
+//     return Array.from({length:n}).reduce(
+//         (acc, val, i) => acc.concat( i>1 ? acc[i-1] + acc[i-2] : i),
+//         []
+//     )
+// };
 
-};
+const fibonacci = function(num){
+    const n = parseInt(num)
+    return n < 0? 'OOPS'
+            : n <=2 ? 1
+            : fibonacci (n-1) + fibonacci(n-2)
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
